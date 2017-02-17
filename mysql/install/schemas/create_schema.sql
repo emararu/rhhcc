a8b1bc461add2ad2656c0b6c585cf9de87789516
emararu <emararu@gmail.com>
2017-02-17 11:11:45 +0300

    Mysql: процедура подтверждения регистрации пользователя

--- mysql/install/schemas/create_schema.sql
+++ mysql/install/schemas/create_schema.sql
@@ -1,8 +1,8 @@
--- 
+﻿-- 
 -- Выполняет создание схемы БД
 -- 
 
-set @sql_create_schema = concat('create schema ', @SCHEMA_NAME, ' character set utf8 collate utf8_general_ci');
+set @sql_create_schema = concat('create schema ', @SCHEMA_NAME, ' character set utf8mb4 collate utf8mb4_unicode_ci');
 prepare stmt_create_schema from @sql_create_schema;
 execute stmt_create_schema;
 deallocate prepare stmt_create_schema;
